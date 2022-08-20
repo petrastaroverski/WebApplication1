07791a4aa3d4b77a25a77352795d924dcd8876c3
petrastaroverski <80577969+petrastaroverski@users.noreply.github.com>
2022-08-20 21:16:18 +0200

    Add files via upload

---
+++ WebApplication1/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApplication1.Areas.Identity.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+
+    public DbSet<WebApplication1.Models.Note>? Note { get; set; }
+
+    public DbSet<WebApplication1.Models.Person>? Person { get; set; }
+}
+
+internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        //throw new NotImplementedException();
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
+    }
+}
