7e0d230c32b0e21417c4098fbb9cb78607092b0c
petrastaroverski <80577969+petrastaroverski@users.noreply.github.com>
2022-08-25 20:27:47 +0200

    Update ApplicationUser.cs

--- WebApplication1/Areas/Identity/Data/ApplicationUser.cs
+++ WebApplication1/Areas/Identity/Data/ApplicationUser.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -10,8 +11,11 @@ namespace WebApplication1.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public virtual IEnumerable<Person> People { get; set; }
 }
+
 
