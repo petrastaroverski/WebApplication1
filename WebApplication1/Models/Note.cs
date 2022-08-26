ba7bbc20d3dfb4814611e2df6fa007d5cb454e5e
petrastaroverski <80577969+petrastaroverski@users.noreply.github.com>
2022-08-26 18:11:50 +0200

    Update Note.cs

--- WebApplication1/Models/Note.cs
+++ WebApplication1/Models/Note.cs
@@ -6,6 +6,8 @@ namespace WebApplication1.Models
     {
         [Key]
         public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime Date { get; set; } = DateTime.Now;
         public string Summary { get; set; }
     }
